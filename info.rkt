676a1a9ded02c8934909fef6862cdc623c1e7f29
Lehi Toskin <lehi@tosk.in>
2014-06-29 15:45:01 -0700

    Forgot to update info

--- info.rkt
+++ info.rkt
@@ -1,9 +1,9 @@
 #lang setup/infotab
-(define name "OpenAL")
+(define name "libopenal-racket")
 (define blurb
   '("OpenAL is an audio library for Linux, Mac OSX, and Windows commonly used in games. This package provides some simple Racket bindings."))
 (define primary-file "main.rkt")
 (define categories '(media))
 (define repositories '("4.x"))
-(define scribblings '(("openal.scrbl" ())))
+(define scribblings '(("doc/libopenal-racket.scrbl" ())))
 (define release-notes '((p "Fixed up provides, added documentation for libvorbisfile.")))
